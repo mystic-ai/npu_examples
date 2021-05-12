d2d60e58644c3e032b8b0d30c58cf7f379f3f48c
Paul Hetherington <paul@neuro-ai.co.uk>
2021-05-12 21:49:05 +0100

    updates

--- model_hub/vision/clip/python-example.py
+++ model_hub/vision/clip/python-example.py
@@ -8,18 +8,17 @@ import npu
 
 npu.api(os.environ["API_TOKEN"], deployed=True)
 
+#npu_model = npu.compile('input.jpg', library="FILE", model_label="clip", scripts=["clip_prediction_srv"])
+
 image = "input.jpg"
 
 encoded_string = ""
 with open(image, "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
 
-result_response = npu.predict('609ba35e0421e5d2d7052e9f',[[encoded_string]], input_kwargs={"render_boxes": True})[0]
+input = [[encoded_string]]
+phrases = ["a diagram", "of photo of a dog", "a horse"]
 
-result = result_response.encode('latin-1')
-imgdata = base64.b64decode(result)
+result_response = npu.predict('609c32318b6a667ad9b2d8e5', input, input_kwargs={"phrases": phrases})
 
-stream = BytesIO(imgdata)
-
-image = Image.open(stream).convert("RGBA")
-image.show()
+print(result_response)
--- model_hub/vision/detectron2-bounding-boxes/python-example.py
+++ model_hub/vision/detectron2-bounding-boxes/python-example.py
@@ -17,9 +17,30 @@ with open(image, "rb") as image_file:
 result_response = npu.predict('609ba35e0421e5d2d7052e9f',[[encoded_string]], input_kwargs={"render_boxes": True})[0]
 
 result = result_response.encode('latin-1')
+
 imgdata = base64.b64decode(result)
 
 stream = BytesIO(imgdata)
 
 image = Image.open(stream).convert("RGBA")
 image.show()
+
+# Rendering in HTML example
+
+with open("response.html", 'w') as file_html:
+    file_html.write(
+        '''
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>title</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <img src="data:image/jpg;base64, %s" alt="Fail!!"/>
+  </body>
+</html>
+        ''' % result_response
+    )
