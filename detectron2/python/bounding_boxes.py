2054a3ee80fb386678da6585c42877cc4a07f391
Paul Hetherington <paul@neuro-ai.co.uk>
2021-05-12 10:51:37 +0100

    working detectron2

--- detectron2/python/bounding_boxes.py
+++ detectron2/python/bounding_boxes.py
@@ -1,9 +1,12 @@
 import os
 import base64
 
+from PIL import Image
+from io import BytesIO
+
 import npu
 
-npu.api(os.environ["API_TOKEN"], deployed=False)
+npu.api(os.environ["API_TOKEN"], deployed=True)
 
 image = "input.jpg"
 
@@ -11,5 +14,16 @@ encoded_string = ""
 with open(image, "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
 
-npu.predict('609a8ec379000ea09ac20525',[[encoded_string]], input_kwargs={"render_boxes": True})
+result_response = npu.predict('609ba35e0421e5d2d7052e9f',[[encoded_string]], input_kwargs={"render_boxes": True})[0]
 
+result = result_response.encode('latin-1')
+imgdata = base64.b64decode(result)
+
+stream = BytesIO(imgdata)
+
+image = Image.open(stream).convert("RGBA")
+image.show()
+
+filename = 'result_image.jpeg'
+with open(filename, 'wb') as f:
+    f.write(imgdata)
