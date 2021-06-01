06c65ef1502f3285533057d7317a3ef3cbe9aa0b
oscar <oscar.roviramontenegro@gmail.com>
2021-06-01 17:10:43 +0100

    Added gpt-neo->B

---
+++ model_hub/gpt-neo-1-3B-text-generation/python-example.py
@@ -0,0 +1,14 @@
+import os
+import npu
+
+npu.api(os.environ["API_TOKEN"], deployed=True)
+
+model_id = '60a40f81b635d67d627bf6b2'
+data = ['When I visit Bath I will']
+
+kwargs = {
+    'max_length':15
+}
+
+output = npu.predict(model_id, data, kwargs)
+print(output)
