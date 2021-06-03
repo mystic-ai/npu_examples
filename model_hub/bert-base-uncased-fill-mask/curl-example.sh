97a56746dc848c28f39e73e67b2c4c576b94ddb8
oscar <oscar.roviramontenegro@gmail.com>
2021-06-03 12:23:00 +0100

    Removed brackets

--- model_hub/bert-base-uncased-fill-mask/curl-example.sh
+++ model_hub/bert-base-uncased-fill-mask/curl-example.sh
@@ -3,4 +3,4 @@ curl -X POST "https://api.neuro-ai.co.uk/SyncPredict?include_result=true" \
      -H "Accept:application/json" \
      -H "Content-Type: application/json" \
      -H "Authorization: Bearer $API_TOKEN" \
-     -d '{"modelId":"60a3c2a00421e5d2d7053ab9","data": ["The river is [MASK]."], "input_kwargs": {"top_k":3}}'
+     -d '{"modelId":"60a3c2a00421e5d2d7053ab9","data": "The river is [MASK].", "input_kwargs": {"top_k":3}}'
