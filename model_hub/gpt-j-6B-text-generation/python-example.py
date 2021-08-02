5935c966df33c666f24fe396cc408d8c0d5df6a1
William Eliot <75908561+wjeliot@users.noreply.github.com>
2021-08-02 15:58:17 +0100

    add new parameters

--- model_hub/gpt-j-6B-text-generation/python-example.py
+++ model_hub/gpt-j-6B-text-generation/python-example.py
@@ -7,7 +7,13 @@ model_id = '60ca2a1e54f6ecb69867c72c'
 data = ['When I visit Bath I will']
 
 kwargs = {
-    'max_length':15
+    'max_new_tokens': 25, # how many new tokens to generate REQUIRED
+    # 'max_length': 160,
+    # 'penalty': 1.0,
+    # 'top_p': 0.92,
+    # 'top_k': 0,
+    # 'temperature': 0.8,
+    # 'remove_input': False
 }
 
 output = npu.predict(model_id, data, kwargs)
