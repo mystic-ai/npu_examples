4ed11e20030f236cc501e86c6f811d5a7d0f6fe7
William Eliot <75908561+wjeliot@users.noreply.github.com>
2021-08-04 14:48:05 +0100

    feat: repair string input and change kwarg

--- model_hub/gpt-j-6B-text-generation/javascript-example.js
+++ model_hub/gpt-j-6B-text-generation/javascript-example.js
@@ -9,7 +9,7 @@ fetch("https://api.neuro-ai.co.uk/SyncPredict?include_result=true", {
   },
   body: JSON.stringify({
     modelId: "60ca2a1e54f6ecb69867c72c",
-    data: ["When I visit Bath I will"],
-    input_kwargs: { max_length: 15 },
+    data: "When I visit Bath I will",
+    input_kwargs: { max_new_tokens: 15 },
   }),
 });
